ab29a6f1cb1d26c188cba7c3cc1036a7e27cad3f
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-05 22:30:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "lavanya-bucket001"
-    key            = "uc6/terraform.tfstate"
+    bucket         = "my-oidc-bucket-15328069840"
+    key            = "two-tier-app/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    use_lockfile   = true
+    use_lockfile   = false
   }
 }
